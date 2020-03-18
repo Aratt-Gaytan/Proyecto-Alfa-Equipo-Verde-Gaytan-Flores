c6e29e33d2aaf45d78e7324ea242a90f4c3658cb
Aratt-Gaytan <60763551+Aratt-Gaytan@users.noreply.github.com>
2020-03-18 13:14:16 -0600

    Add files via upload
    actividad de 12/03/20

--- formulario/conexion.php
+++ formulario/conexion.php
@@ -1,31 +1,78 @@
 <?php
- 	$host ="localhost";
- 	$user ="root";
- 	$pass ="";
+$host ="localhost";
+$user ="root";
+$pass ="";
 
- 
- 	
- 	$con=mysqli_connect($host,$user,$pass)or die("Error al Conectar");
- 	mysqli_select_db($con,'lagrande')or die("No de pudo conectar con la base de datos ERROR 404 NOT FOUND");
 
- 	
 
- 	
-	$nom=$_POST["nombre"];
- 	$app=$_POST["apellidopaterno"];
- 	$apm=$_POST["apellidomaterno"];
- 	$co=$_POST["correo"];
- 	$tel=$_POST["telefono"];
- 	$ejecuta=mysqli_query($con,"insert into usuarios (nombre,ApellidoPaterno,ApellidoMaterno,Correo,Telefono) values ('$nom','$app','$apm','$co','$tel')");
-	if (!$ejecuta) {
- 		echo "Hubo un error al insertar los datos";
- 	}
- 		else{
- 			echo "Los datos fueron insertados correctemente";
- 		}
+$con=mysqli_connect($host,$user,$pass)or die("Error al Conectar");
+mysqli_select_db($con,'sucursaldeclientes')or die("No de pudo conectar con la base de datos ERROR 404 NOT FOUND");
 
 
 
 
+$busqueda=$_POST["busqueda"];
+$nombre=$_POST["nombre"];
+$xd=1;
 
- ?>
+if ($busqueda == "clientes"){
+	$sql = "select * from clientes where nombre = '$nombre'";
+
+	$result = mysqli_query($con, $sql);
+
+	$num = mysqli_num_rows($result);
+	if ($num==0){
+		echo "non hay nada unu";
+		}else{
+
+		// output data of each row
+			while ( $row = mysqli_fetch_assoc($result)) {
+
+				echo "Nombre: ".$row["nombre"]."<br>"; 
+				echo "Primer apellido: ".$row["primer_apellido"]."<br>";
+				echo "Segundo apellido: ".$row["segundo_apellido"]."<br>";     
+				echo "Edad: ".$row["edad"]."<br>";
+				echo "Celular: ".$row["celular"]."<br>"; 
+				echo "Sucursal a la que pertenece: ".$row["sucursal"]."<br>";
+
+		}
+
+	} 
+
+}else{ 
+if ($busqueda == "sucursal"){
+	$sql = "select nombre,primer_apellido from clientes where sucursal = '$nombre'";
+
+	$result = mysqli_query($con, $sql);
+
+	$num = mysqli_num_rows($result);
+	if ($num==0){
+		echo "non hay nada unu";
+		}else{
+		echo "Clientes de sucursal ".$nombre."<br>";
+		// output data of each row
+		while ( $row = mysqli_fetch_assoc($result)) {
+
+			echo "Nombre: ".$row["nombre"]."<br>"; 
+			echo "Primer apellido: ".$row["primer_apellido"]."<br>";
+			echo "=================================================="."<br>";
+		} 
+	}
+
+
+	}else{
+
+	echo "selecciona que deseas buscar";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+?>
